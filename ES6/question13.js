c4d7bd84e48b5a88b260b607e0758ef3d8c08f35
Jivak Sute <jivakysute@gmail.com>
2025-05-31 19:19:56 +0530

    es6 question 13 done.

---
+++ ES6/question13.js
@@ -0,0 +1,18 @@
+const profile = {
+  name: "Charlie",
+  age: 29,
+  address: { city: "San Francisco", zipcode: "94101" },
+};
+
+const updates = { age: 30, address: { zipcode: "94109", country: "USA" } };
+
+const mergedObj = {
+  ...profile,
+  ...updates,
+  address: {
+    ...profile.address,
+    ...updates.address,
+  },
+};
+
+console.log(mergedObj);
