6703538216dd185e337f5e9514c2387831e41863
Jivak Sute <jivakysute@gmail.com>
2025-06-10 15:38:32 +0530

    MongoDB Aggregations Questions 9 completed.

--- MONGODB-AGGREGATION/Q9/routes/analyticsRoutes.js
+++ MONGODB-AGGREGATION/Q9/routes/analyticsRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const analyticsRouter = express.Router();
 const Attendance = require("../models/attendance");
-const mongoose = require("mongoose");
 
 analyticsRouter.get("/analytics/total-attendance", async (req, res) => {
   try {
