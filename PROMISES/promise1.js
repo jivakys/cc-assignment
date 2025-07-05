ad89208503831c4a974c30ede19742b37b30814e
Jivak Sute <jivakysute@gmail.com>
2025-07-05 19:21:43 +0530

    Promises Questions.

--- PROMISES/promise1.js
+++ PROMISES/promise1.js
@@ -1,7 +1,7 @@
-function timer(duration, onComplete) {
+function timer(dur, onComplete) {
   setTimeout(() => {
-    onComplete(`Timer of ${duration} ms finished`);
-  }, duration);
+    onComplete(`Timer of ${dur} ms finished`);
+  }, dur);
 }
 
 timer(2000, (message) => {
