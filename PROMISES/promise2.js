18d155f0c353e65a52c24f7fe6000c514d17af8f
Jivak Sute <jivakysute@gmail.com>
2025-07-25 18:10:15 +0530

    promises questions.

--- PROMISES/promise2.js
+++ PROMISES/promise2.js
@@ -20,3 +20,26 @@ async function fetchDatahandler() {
 }
 
 fetchDatahandler();
+
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() > 0.5) {
+        resolve("Fetched data successfully!");
+      } else {
+        reject("data not found");
+      }
+    });
+  }, 1000);
+}
+
+async function fetchDatahandler() {
+  try {
+    const result = await fetchData();
+    console.log(result);
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+}
+
+fetchDatahandler();
