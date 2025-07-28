66e998e8b391fff72651d8c20646b269f8b5f22a
Jivak Sute <jivakysute@gmail.com>
2025-07-28 19:23:00 +0530

    Constructor question 11 done.

--- CONSTRUCT0R/question11.js
+++ CONSTRUCT0R/question11.js
@@ -9,5 +9,5 @@ function createEmployee(name, role, salary) {
   };
 }
 
-const employee1 = createEmployee("Jalaluddin", "Developer", 6000);
+const employee1 = createEmployee("Jalaluddin", "Business man", 6000);
 employee1.introduce();
